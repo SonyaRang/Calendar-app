9c94c2c903b7af866c2aac5d3f820e1d1f400a87
Sonz <sonyarangraj@msn.com>
2021-04-25 22:35:56 -0500

    java changes

---
+++ js/java.js
@@ -0,0 +1,16 @@
+var saveButton = document.querySelector(".save-btn").addEventListener("click", function(){
+    console.log("test");
+
+    //save input text to local storage//
+
+    //retreave local storeage//
+
+    //input retreaved loal storage back to line
+    })
+    
+    var colorTime = document.querySelectorAll (".description");
+    var colorTimeArray = Array.from (colorTime);
+    console.log (colorTimeArray);
+    //write a function to loop through color time array
+    //write an if statement to check current time against the list object time and return true or false
+    //each if statement should determine wether to add class of past/present/future to the todo element
